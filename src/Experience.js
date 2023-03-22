420a8c000188422ac0e3897a14f5292b61756da6
LuisaCardenasL <luisa072697@gmail.com>
2023-03-22 17:36:58 -0500

    se corrigen errores

--- src/Experience.js
+++ src/Experience.js
@@ -27,7 +27,6 @@ export function Experience(){
     
     return (
       <>
-        //
         <OrbitControls
           enableRotate={true}
           enablePan={false}
@@ -36,22 +35,22 @@ export function Experience(){
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 3, 3]} intensity={1.5} />
         <Center>
-          //box
+          
           <mesh ref={boxRef}>
             <boxGeometry args={[1, 1, 1]} />
             <meshStandardMaterial color={"mediumvioletred"} />
           </mesh>
-          //sphere
+          
           <mesh ref={sphereRef} position-x={-3}>
             <sphereGeometry args={[1, 32, 64]} />
             <meshStandardMaterial color={"violet"} />
           </mesh>
-          //cone
+          
           <mesh ref={coneRef} position-x={3}>
             <coneGeometry args={[1, 1, 10]} />
             <meshStandardMaterial color={"purple"} />
           </mesh>
-          //floor
+          
           <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
             <planeGeometry args={[]} />
             <meshStandardMaterial color={"mediumpurple"} />
