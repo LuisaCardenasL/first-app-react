2ef71c0b7c2f469cef03dd4c6ba6b846876329c6
LuisaCardenasL <luisa072697@gmail.com>
2023-03-22 17:23:41 -0500

    se termina el proyecto

--- src/Experience.js
+++ src/Experience.js
@@ -10,43 +10,51 @@
 import { Center, OrbitControls, PointerLockControls, PresentationControls } from "@react-three/drei";
 import { useFrame, useThree} from "@react-three/fiber"
 import { useRef } from "react";
-import { ConeGeometry } from "three";
 
 export function Experience(){
+
+    const { camera, gl } = useThree();
+
     const boxRef = useRef()
     const sphereRef = useRef()
     const coneRef = useRef();
-
-    // const {camera, gl} = useThree()
    
     useFrame((state, delta)=>{
         boxRef.current.rotation.x += 1 * delta  ;
-        sphereRef.current.rotation.x += 10 * delta;
-        coneRef.current.rotation.z += 10 * delta;
+        sphereRef.current.rotation.x += 3 * delta;
+        coneRef.current.rotation.z += 1 * delta;
     })
     
     return (
       <>
-        <OrbitControls enableRotate={true} />
-        <ambientLight />
-
+        //
+        <OrbitControls
+          enableRotate={true}
+          enablePan={false}
+          makeDefault={true}
+        />
+        <ambientLight intensity={0.5} />
         <directionalLight position={[10, 3, 3]} intensity={1.5} />
         <Center>
+          //box
           <mesh ref={boxRef}>
             <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={"mediumpurple"} />
+            <meshStandardMaterial color={"mediumvioletred"} />
           </mesh>
+          //sphere
           <mesh ref={sphereRef} position-x={-3}>
             <sphereGeometry args={[1, 32, 64]} />
             <meshStandardMaterial color={"violet"} />
           </mesh>
+          //cone
           <mesh ref={coneRef} position-x={3}>
             <coneGeometry args={[1, 1, 10]} />
             <meshStandardMaterial color={"purple"} />
           </mesh>
+          //floor
           <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
             <planeGeometry args={[]} />
-            <meshStandardMaterial color={"lavender"} />
+            <meshStandardMaterial color={"mediumpurple"} />
           </mesh>
         </Center>
       </>
